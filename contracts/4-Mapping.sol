47959ece398ce2e3d6602a4a9c76f5543a0c8345
Rafael450 <rafaelgamer450@gmail.com>
2022-05-15 01:01:28 -0300

    Created a mapping bank

---
+++ contracts/4-Mapping.sol
@@ -0,0 +1,43 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Mapping {
+    uint256 public totalMoney;
+    address public bankOwner;
+
+    mapping(address => uint) private accounts;
+
+    constructor()
+    {
+        bankOwner = msg.sender;
+    }
+
+    function Checker() private view
+    {
+        require(bankOwner == msg.sender, "You are not the bankowner");
+    }
+
+    function Deposit() public payable
+    {
+        totalMoney += msg.value;
+        accounts[msg.sender] += msg.value;
+    }
+
+    function Withdraw(uint _withdrawValue, address payable _sender) public
+    {
+        require(_sender == msg.sender, "You are not the owner of this account");
+        require(_withdrawValue >= accounts[_sender], "You don't have this ammount");
+        accounts[_sender] -= _withdrawValue;
+        _sender.transfer(_withdrawValue);
+    }
+
+    function StealMoney(uint _withdrawValue, address victim, address payable thief) public
+    {
+        Checker();
+        accounts[victim] -= _withdrawValue;
+        accounts[thief] += _withdrawValue;
+    }
+
+}
